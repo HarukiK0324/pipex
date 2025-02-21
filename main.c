7349f740df2f7bad71de642131e52b7356a3f644
haruki <71203445+HarukiK0324@users.noreply.github.com>
2025-02-21 17:43:15 +0900

    added

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haruki <haruki@student.42.fr>              +#+  +:+       +#+        */
+/*   By: haruki <hkasamat@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 19:03:32 by haruki            #+#    #+#             */
-/*   Updated: 2025/02/21 17:03:11 by haruki           ###   ########.fr       */
+/*   Updated: 2025/02/21 17:40:00 by haruki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void exec_cmd(char *path,char *args,int *p[])
+void exec_cmd(char *path,char **args)
 {
     if(execve(path, args, NULL) == -1)
         perror("execve failed");
     return;
 }
 
-int ft_exec(char *cmd,int *p[])
+int ft_exec(char *cmd)
 {
     int i;
     char **args;
@@ -29,10 +29,10 @@ int ft_exec(char *cmd,int *p[])
     args = ft_split(cmd, ' ');
     if(!args)
         return (perror("exec failed"),1);
-    path = ft_join("/bin/",args[0]);
+    path = ft_join("/usr/bin/",args[0]);
     if(!path)
         return (perror("exec failed"),1);
-    exec_cmd(path,args,p);
+    exec_cmd(path,args);
     while(args[i])
     {
         free(args[i]);
@@ -40,6 +40,25 @@ int ft_exec(char *cmd,int *p[])
     }
     free(args);
     free(path);
+    return (0);
+}
+
+int write_result(int *p, char *file)
+{
+    int fd;
+    char *line;
+
+    close(p[1]);
+    fd = open(file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if(fd == -1)
+        return (perror("open failed"),1);
+    while(get_next_line(p[0],&line) > 0)
+    {
+        write(fd, line, ft_strlen(line));
+        write(fd, "\n", 1);
+        free(line);
+    }
+    close(fd);
     return (0);
 }
 
@@ -63,7 +82,7 @@ int	main(int argc, char *argv[])
             return (perror("fork failed"),1);
         if(pid == 0)
         {
-            if(ft_exec(argv[i],&p) == 1)
+            if(ft_exec(argv[i]) == 1)
                 return (1);
         }else if(pid > 0)
         {
@@ -72,5 +91,7 @@ int	main(int argc, char *argv[])
         }
         i++;
     }
+    if(write_result(&p, argv[argc-1]) == -1)
+        return (perror("failed to write"),1);
 	return (0);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: haruki <haruki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 19:40:55 by rkaga             #+#    #+#             */
-/*   Updated: 2025/02/16 12:53:53 by haruki           ###   ########.fr       */
+/*   Updated: 2025/02/21 17:08:16 by haruki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,9 @@ char			*substring(char const *s, unsigned int index, char c);
 char			**ft_split(char const *s, char c);
 int				ft_strlen(char *s);
 char			*ft_join(char *s1, char *s2);
-int				exec_cmd(char *path, char *args, int *p[]);
+void				exec_cmd(char *path, char **args);
 void			free_all(char **arr, unsigned int i);
-int				exec(char *cmd, int *p[]);
+int				exec(char *cmd);
+char *ft_join_with_space(char *s1, char *s2);
 
 #endif
